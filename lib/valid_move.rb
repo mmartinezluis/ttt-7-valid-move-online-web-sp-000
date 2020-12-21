7be77f0082ca004101fa6069141f1e3e4351ebb4
Luis Martinez <luis_mmartinez@live.com>
2020-12-21 01:14:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,16 @@
 # code your #valid_move? method here
 
+def valid_move?(board,index)
+  (0..8).include?(index) && !position_taken?(board,index)
+end
+
+
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board,index)
+  if board[index]==" "
+    return false
+  else
+    return true
+  end
+end
